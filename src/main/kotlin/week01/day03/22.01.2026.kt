b32f58a420a9b7561c68d8b780e93fb77d2fd43e
Dmitry <slomov.dima@mail.ru>
2026-01-23 01:21:41 +0300

    22.01.2026.kt (надо доделать, не успеваю)

--- src/main/kotlin/week01/day03/22.01.2026.kt
+++ src/main/kotlin/week01/day03/22.01.2026.kt
@@ -34,6 +34,22 @@ data class Garage(
     val cars: List<Car>
 )
 
+// #3
+fun addMileage(car: Car, km: Int): Car {
+    return car.copy(mileageKm = car.mileageKm + km)
+}
+
+
+fun sellCar(car: Car, newOwner: Owner?): Car {
+    return car.copy(owner = newOwner)
+}
+
+
+fun upgradeEngine(car: Car, hpDelta: Int): Car {
+    return car.copy(engine = car.engine.copy(hp = car.engine.hp + hpDelta))
+}
+
+
 fun main() {
 
     // #2
@@ -44,5 +60,25 @@ fun main() {
     println(testHashSet1.size) // 1
     val testHashMap1 = hashMapOf<Car, Int>()
     testHashMap1[car1] = 999
-    println(testHashMap1[car2])
+    println(testHashMap1[car2]) // 999
+
+    // #3
+    println("car1 = ${addMileage(car1, 100)}")
+    println("car2 = $car2")
+
+    val mishaOwner = Owner("Misha", experienceYears = 2)
+    val sold = sellCar(car1, mishaOwner)
+    println("old car1 = $car1")
+    println("new car  = $sold")
+
+    println("upgrade engine ${upgradeEngine(car1, 197)}")
+    println("car1 = $car1")
+
+    // #4
+    val b47 = Engine(190, 2.0, "D")
+    val (hp, volume, type) = b47
+    val b47Hp = b47.component1()
+    val b47Volume = b47.component2()
+    val b47Type = b47.component3()
+
 }
