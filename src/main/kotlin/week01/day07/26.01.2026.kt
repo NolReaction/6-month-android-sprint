c040172ba1bba645f3f8e1da3223c3cae5f1efe6
Dmitry <slomov.dima@mail.ru>
2026-01-27 12:51:37 +0300

    26.01.2026.kt (изменил базу, сделал addTask)

--- src/main/kotlin/week01/day07/26.01.2026.kt
+++ src/main/kotlin/week01/day07/26.01.2026.kt
@@ -4,9 +4,15 @@ import java.time.Instant
 import java.time.Duration
 
 val aliases = mapOf(
-    "exit" to "exit", "quit" to "exit",
+    "help" to "help", "h" to "help",
+    "list" to "list", "l" to "list",
+    "add" to "add", "ad" to "add",
+    "find" to "find", "f" to "find",
+    "start" to "start", "star" to "start",
+    "done" to "done",
     "del" to "del", "delete" to "del", "dl" to "del",
-    "help" to "help", "h" to "help"
+    "stat" to "stat",
+    "exit" to "exit", "quit" to "exit"
 )
 
 fun helloMessage() {
@@ -38,16 +44,16 @@ sealed class TaskStatus {
     object New : TaskStatus()
 
     data class InProgress(
-        val startedAt: Instant
+        val startedAt: Instant?
     ) : TaskStatus()
 
     data class Done(
-        val finishedAt: Instant,
-        val spentTime: Duration
+        val finishedAt: Instant?,
+        val spentTime: Duration?
     ) : TaskStatus()
 
     data class Removed(
-        val removedAt: Instant,
+        val removedAt: Instant?,
         val reason: String?
     ) : TaskStatus()
 }
@@ -60,27 +66,76 @@ data class Task(
     val status: TaskStatus
 )
 
-fun addTask() {
+fun addTask(taskList: MutableList<Task>): Task {
+    // Выдаём новое id
+    val id = nextId(taskList)
+
+    // Выдаём title
+    val title = run {
+        while (true) {
+            print("Title > ")
+            val t = readln().trim()
+            if (t.isBlank()) {
+                println("Print Title")
+                continue
+            }
+            return@run t
+        }
+        error("Unreachable")
+    }
+
+
+    // Выдаём priority
+    val priority: Priority
+    while (true) {
+        println(
+            """
+        Choose a priority:
+            1.HIGH
+            2.MEDIUM
+            3.LOW
+    """.trimIndent()
+        )
+        val response = readln().trim()
+        priority = when(response) {
+            "1" -> Priority.HIGH
+            "2" -> Priority.MEDIUM
+            "3" -> Priority.LOW
+            else -> { println("Usage: 1 / 2 / 3"); continue }
+        }
+        break
+    }
+
+    // Выдаём notes (заметку)
+    print("Note > ")
+    val notes = readln().trim().ifEmpty { null }
+
+    // Выдаём статус
+    val status = TaskStatus.New
+
+    // Добавляю новый task в хранилище и возвращаю task
+    val task = Task(id, title, priority, notes, status)
+    taskList.add(task)
+    return task
+}
+
+fun listTasks(taskList: List<Task>) {
+    for (task in taskList) println(task)
+}
+
+fun showTasksById(id: Int?) {
 
 }
 
-fun listTasks() {
+fun startTask(id: Int?) {
 
 }
 
-fun showTasksById() {
+fun doneTask(id: Int?) {
 
 }
 
-fun startTask() {
-
-}
-
-fun doneTask() {
-
-}
-
-fun deleteTask() {
+fun deleteTask(id: Int?) {
 
 }
 
@@ -88,28 +143,43 @@ fun statisticTask() {
 
 }
 
+fun nextId(taskList: List<Task>): Int {
+    val maxId = taskList.maxOfOrNull { it.id } ?: -1
+    return maxId + 1
+}
+
 fun main() {
     helloMessage()
 
+    // Хранение данных
+    val taskList = mutableListOf<Task>(
+        Task(0, "First task", Priority.HIGH, null, TaskStatus.Done(null, null))
+    )
+
     while (true) {
         print("> ")
+
+        // Ввод начальной команды
         val line = readln().trim()
         if (line.isEmpty()) continue
-
         val parts = line.split(Regex("\\s+"))
         val rawCmd = parts.getOrNull(0)?.lowercase() ?: ""
         val cmd = aliases[rawCmd] ?: rawCmd // command (example add)
         val arg = parts.getOrNull(1) // id (example - 12)
+        val id = arg?.toIntOrNull()
 
         when (cmd) {
             "help" -> helloMessage()
             "exit" -> break
-            "list" -> listTasks()
-            "add" -> addTask()
-            "find" -> showTasksById(/* id */)
-            "start" -> startTask(/* id */)
-            "done" -> doneTask(/* id */)
-            "del" -> deleteTask(/* id */)
+            "list" -> listTasks(taskList)
+            "add" -> addTask(taskList).also {
+                println("A new task has been created:")
+                println(it)
+            }
+            "find" -> if (id == null) println("Usage: find <id>") else showTasksById(id)
+            "start" -> if (id == null) println("Usage: start <id>") else startTask(id)
+            "done" -> if (id == null) println("Usage: done <id>") else doneTask(id)
+            "del" -> if (id == null) println("Usage: del <id>") else deleteTask(id)
             "stat" -> statisticTask()
             else -> println("Unknown command: $rawCmd")
         }
